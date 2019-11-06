819c0b208f11ca49b81ba4bbe1a680ed96331b62
jeroen.verwimp <jeroen.verwimp@student.pxl.be>
2019-11-06 19:33:15 +0000

    Merged PR 6: Setup web project structure and layout
    Created a base structure for the angular project (web panel) and added a basic layout.
    
    Issue: P1920PL17-179

---
+++ Signawel.Web/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: HomeComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
