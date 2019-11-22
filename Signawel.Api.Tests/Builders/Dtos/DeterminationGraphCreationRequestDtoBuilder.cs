75276c0e903ccee5d69bf02ba26f4017fb0067f8
jonas.reniers <jonas.reniers@student.pxl.be>
2019-11-22 14:28:02 +0000

    Merged PR 35: Added determination graph tests
    https://jirastudenten.pxl.be/browse/P1920PL17-250

---
+++ Signawel.Api.Tests/Builders/Dtos/DeterminationGraphCreationRequestDtoBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Signawel.Dto.Determination;
+
+namespace Signawel.Api.Tests.Builders.Dtos
+{
+    public class DeterminationGraphCreationRequestDtoBuilder
+    {
+        private readonly DeterminationGraphCreationRequestDto _determinationGraphCreationRequestDto;
+
+        public DeterminationGraphCreationRequestDtoBuilder()
+        {
+            _determinationGraphCreationRequestDto = new DeterminationGraphCreationRequestDto();
+        }
+
+        public DeterminationGraphCreationRequestDtoBuilder WithStart()
+        {
+            _determinationGraphCreationRequestDto.Start = new DeterminationNodeCreatingRequestDto();
+            return this;
+        }
+
+        public DeterminationGraphCreationRequestDto Build()
+        {
+            return _determinationGraphCreationRequestDto;
+        }
+    }
+}
---
+++ Signawel.Api.Tests/Controllers/DeterminationGraphControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Signawel.API.Controllers;
+using Signawel.Api.Tests.Builders.Dtos;
+using Signawel.Business.Abstractions.Services;
+using Signawel.Dto.Determination;
+
+namespace Signawel.Api.Tests.Controllers
+{
+    public class DeterminationGraphControllerTests
+    {
+        private DeterminationGraphController _determinationGraphController;
+        private Mock<IDeterminationService> _determinationServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _determinationServiceMock = new Mock<IDeterminationService>();
+            _determinationGraphController = new DeterminationGraphController(_determinationServiceMock.Object);
+        }
+
+        [Test]
+        public void GetDeterminationGraphShouldReturnOkObject()
+        {
+            // Arrange
+            var determinationGraphResponseDto = new DeterminationGraphResponseDto();
+
+            _determinationServiceMock.Setup(service => service.GetGraphAsync())
+                .ReturnsAsync(determinationGraphResponseDto);
+
+            // Act
+            var result = _determinationGraphController.GetDeterminationGraph().Result as OkObjectResult;
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Value, Is.EqualTo(determinationGraphResponseDto));
+            _determinationServiceMock.Verify(service => service.GetGraphAsync(), Times.Once);
+        }
+
+        [Test]
+        public void SetDeterminationGraphWithValidDtoShouldReturnOkObject()
+        {
+            // Arrange
+            var determinationGraphRequestDto = new DeterminationGraphCreationRequestDtoBuilder().WithStart().Build();
+            var determinationGraphResponseDto = new DeterminationGraphResponseDto();
+
+            _determinationServiceMock.Setup(service => service.SetGraphAsync(It.IsAny<DeterminationGraphCreationRequestDto>()))
+                .ReturnsAsync(determinationGraphResponseDto);
+
+            // Act
+            var result = _determinationGraphController.SetDeterminationGraph(determinationGraphRequestDto);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            _determinationServiceMock.Verify(service => 
+                service.SetGraphAsync(determinationGraphRequestDto), Times.Once);
+        }
+    }
+}
